6ea7a0ed913164c08f52d06900e0f8d5071f72a6
Alex GALINIER <nawax88@gmail.com>
2013-09-03 10:52:51 +0200

    Style bikes/stands search form

--- src/VelibNRoses/VelibNRoses/src/main/java/com/onelightstudio/velibnroses/MainActivity.java
+++ src/VelibNRoses/VelibNRoses/src/main/java/com/onelightstudio/velibnroses/MainActivity.java
@@ -1,11 +1,23 @@
 package com.onelightstudio.velibnroses;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -25,12 +37,81 @@ import com.onelightstudio.velibnroses.ws.WSSilentHandler;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.security.Timestamp;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
-public class MainActivity extends FragmentActivity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
+public class MainActivity extends FragmentActivity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener, View.OnClickListener {
 
+    class GetAddressTask extends AsyncTask<Location, Void, String> {
+
+        private EditText field;
+        private ImageButton locationButton;
+        private ProgressBar locationProgress;
+        private Location location;
+
+        public GetAddressTask(int fieldId) {
+            Log.e("Field id "+fieldId);
+            switch (fieldId) {
+                case FIELD_DEPARTURE:
+                    field = departureField;
+                    locationButton = departureLocationButton;
+                    locationProgress = departureLocationProgress;
+                    break;
+                case FIELD_ARRIVAL:
+                    field = arrivalField;
+                    locationButton = arrivalLocationButton;
+                    locationProgress = arrivalLocationProgress;
+                    break;
+            }
+        }
+
+        @Override
+        protected String doInBackground(Location... params) {
+            location = params[0];
+            Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            } catch (IOException e1) {
+                return null;
+            } catch (IllegalArgumentException e2) {
+                return null;
+            }
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                String addressText = String.format(
+                        "%s, %s",
+                        address.getMaxAddressLineIndex() > 0 ? address .getAddressLine(0) : "", address.getLocality()
+                );
+                return addressText;
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            locationButton.setVisibility(View.VISIBLE);
+            locationProgress.setVisibility(View.GONE);
+            if (result != null) {
+                field.setText(result);
+            } else {
+                Toast.makeText(MainActivity.this, R.string.address_not_found, Toast.LENGTH_LONG).show();
+                field.setText(location.getLatitude() + "," + location.getLongitude());
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            locationButton.setVisibility(View.GONE);
+            locationProgress.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private static final int FIELD_DEPARTURE = 0;
+    private static final int FIELD_ARRIVAL = 1;
     private final static String FORCE_CAMERA_POSITION = "ForceCameraPosition";
 
     private GoogleMap mMap;
@@ -41,6 +122,16 @@ public class MainActivity extends FragmentActivity implements GooglePlayServices
     private Handler timer;
     private Runnable timeRunnable;
     private Long pausedTime;
+
+    private View searchView;
+    private View mapView;
+    private boolean searchViewVisible;
+    private EditText departureField;
+    private ImageButton departureLocationButton;
+    private ProgressBar departureLocationProgress;
+    private EditText arrivalField;
+    private ImageButton arrivalLocationButton;
+    private ProgressBar arrivalLocationProgress;
 
     @Override
     public void onAttachedToWindow() {
@@ -68,6 +159,36 @@ public class MainActivity extends FragmentActivity implements GooglePlayServices
     }
 
     @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.departure_mylocation_button:
+                fillAddressFieldWithCurrentLocation(FIELD_DEPARTURE);
+                break;
+            case R.id.arrival_mylocation_button:
+                fillAddressFieldWithCurrentLocation(FIELD_ARRIVAL);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                toggleSearchViewVisible();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle pSavedInstanceState) {
         super.onCreate(pSavedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
@@ -78,7 +199,7 @@ public class MainActivity extends FragmentActivity implements GooglePlayServices
 
             @Override
             public void run() {
-                Log.d(this.getClass().getName(), "Refresh Map Tick");
+                Log.d("Refresh Map Tick");
                 if (pausedTime == null) {
                     setMapStationsOnTick();
 
@@ -89,6 +210,17 @@ public class MainActivity extends FragmentActivity implements GooglePlayServices
         timer = new Handler();
         timer.post(timeRunnable);
 
+        //Init view and elements
+        searchView = findViewById(R.id.search_view);
+        mapView = findViewById(R.id.map_view);
+        departureField = (EditText) findViewById(R.id.departure_field);
+        departureLocationButton = (ImageButton) findViewById(R.id.departure_mylocation_button);
+        departureLocationProgress = (ProgressBar) findViewById(R.id.departure_mylocation_progress);
+        arrivalField = (EditText) findViewById(R.id.arrival_field);
+        arrivalLocationButton = (ImageButton) findViewById(R.id.arrival_mylocation_button);
+        arrivalLocationProgress = (ProgressBar) findViewById(R.id.arrival_mylocation_progress);
+
+        //Station request
         mStationsRequestSended = false;
 
         if (pSavedInstanceState != null) {
@@ -166,7 +298,7 @@ public class MainActivity extends FragmentActivity implements GooglePlayServices
 
     private void setMapStationsOnTick() {
         if (stations != null) {
-            Log.d(this.getClass().getName(), "Call stations WS");
+            Log.d("Call station WS");
             stations = null;
             setMapStations();
         }
@@ -200,7 +332,7 @@ public class MainActivity extends FragmentActivity implements GooglePlayServices
     private void setMapStationsResult(JSONObject result) {
         JSONArray stationsJSON = (JSONArray) result.opt("list");
 
-        Log.i(this.getClass().getName(), "Stations received : " + stationsJSON.length() + " stations");
+        Log.i("Stations received : " + stationsJSON.length() + " stations");
 
         stations = new ArrayList<Station>();
         for (int i = 0; i < stationsJSON.length(); i++) {
@@ -221,7 +353,7 @@ public class MainActivity extends FragmentActivity implements GooglePlayServices
         if (stations == null) {
             setMapStationsRequest(pDoInBackbround);
         } else {
-            Log.d(this.getClass().getName(), "Set up position");
+            Log.d("Set up position");
             LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
             LatLng mapCenter = mMap.getCameraPosition().target;
             LatLng userPos = new LatLng(mLocationClient.getLastLocation().getLatitude(), mLocationClient.getLastLocation().getLongitude());
@@ -252,5 +384,27 @@ public class MainActivity extends FragmentActivity implements GooglePlayServices
         double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLng / 2) * Math.sin(dLng / 2) * Math.cos(radLat1) * Math.cos(radLat2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return (long) (Constants.EARTH_RADIUS * c);
+    }
+
+    private void toggleSearchViewVisible() {
+        if (!searchViewVisible) {
+            searchViewVisible = true;
+            mapView.animate().translationY(searchView.getHeight());
+        } else {
+            searchViewVisible = false;
+            mapView.animate().translationY(0);
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(FragmentActivity.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
+    private void fillAddressFieldWithCurrentLocation(int field) {
+        Location userLocation = mLocationClient.getLastLocation();
+
+        if (userLocation == null) {
+            Toast.makeText(this, R.string.location_unavailable, Toast.LENGTH_LONG).show();
+        } else {
+            new GetAddressTask(field).execute(userLocation);
+        }
     }
 }
