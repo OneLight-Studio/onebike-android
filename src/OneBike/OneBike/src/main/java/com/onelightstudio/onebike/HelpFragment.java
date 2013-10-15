d07b8569b0446a3031496885a3c3edf60be0a91b
Tijee <thomas.gallinari@gmail.com>
2013-10-15 09:34:21 +0200

    Add about screen with temporary layout and text

---
+++ src/OneBike/OneBike/src/main/java/com/onelightstudio/onebike/HelpFragment.java
@@ -0,0 +1,18 @@
+package com.onelightstudio.onebike;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by thomas on 14/10/13.
+ */
+public class HelpFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (ViewGroup) inflater.inflate(R.layout.help, container, false);
+    }
+}
